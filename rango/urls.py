1b3d0f5ce9454e4977d865c2c1358fbd55220d5d
Milena Grauzyte (student) <2038286G@student.gla.ac.uk>
2019-02-07 18:28:07 +0000

    6 finished

--- rango/urls.py
+++ rango/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+]
