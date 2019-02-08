145d877a8d56019060341402343380fcdeaed867
Milena Grauzyte <2038286g@student.gla.ac.uk>
2019-02-08 04:00:45 +0000

    9 up to url mapping

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
+
 
 class CategoryForm(forms.ModelForm):
         name = forms.CharField(max_length=128,
@@ -34,4 +36,25 @@ class PageForm(forms.ModelForm):
                         cleaned_data['url'] = url
 
                         return cleaned_data
+                
+class UserForm(forms.ModelForm):
+        password = forms.CharField(widget=forms.PasswordInput())
+
+        class Meta:
+                model = User
+                fields = ('username', 'email', 'pssword')
+
+class UserProfileForm(forms.ModelForm):
+        class Meta:
+                model = UserProfile
+                fields = ('website', 'picture')
+
+
+
+
+
+
+
+
+
                 
